a2b9da70aa19d420af5ed9ed58ed798a29e4b96a
chaerin <achrvv@sookmyung.ac.kr>
2022-01-15 23:04:29 +0900

    2022-01-15

---
+++ kakaoDoll.js
@@ -0,0 +1,34 @@
+function solution(board, moves) {
+  var answer = 0;
+  let bucket = [];
+
+  moves.forEach(move => {
+    const index = board.findIndex(row => row[move - 1] > 0);
+    if (index < 0) return;
+    const doll = board[index][move - 1];
+    console.log(doll);
+    if (!doll) return;
+
+    if ([...bucket].pop() === doll) {
+      bucket.pop();
+      answer += 2;
+    } else {
+      bucket.push(doll);
+    }
+
+    board[index][move - 1] = 0;
+    console.log(board, board[index][move - 1]);
+  });
+  return answer;
+}
+
+solution(
+  [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
+  ],
+  [1, 5, 3, 5, 1, 2, 1, 4]
+);
